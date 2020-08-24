f0c1a8ad3afb7f5be3f620836ab7eafcc0cee0d1
Josh Hewitt <joshua.hewitt@duke.edu>
2020-08-24 12:28:34 -0600

    initial commit

---
+++ drake_graph.R
@@ -0,0 +1,23 @@
+## Load your packages, e.g. library(drake).
+source("./packages.R")
+
+## Load your R files and subplans
+invisible(lapply(list.files("./R", full.names = TRUE, recursive = TRUE), source))
+
+source('R/plan.R')
+
+# r_outdated()
+
+make(the_plan, lock_envir = FALSE, force = TRUE, 
+     targets = paste('validation_statistics', 1:7, sep = '_'))
+
+# make(the_plan, lock_envir = FALSE, force = TRUE, targets='nim_pkg_0.5')
+
+# build graph components
+graph = vis_drake_graph(the_plan, targets_only = TRUE)
+
+# view graph
+visNetwork::visHierarchicalLayout(graph, direction = "LR",
+                                  edgeMinimization = FALSE)
+
+r_make()
