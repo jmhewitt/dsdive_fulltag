ecfe6d28eaead75ff1d4b29ab727b20e68189064
Josh Hewitt <joshua.hewitt@duke.edu>
2020-08-24 18:52:28 -0600

    update plan for full tag fit

--- R/plan.R
+++ R/plan.R
@@ -1,9 +1,11 @@
 lapply(list.files("./R/subplans", full.names = TRUE, recursive = TRUE), source)
 
-the_plan = bind_plans(raw_data_plan, 
-                      depth_bin_imputation_plan,
-                      dive_segmentation_plan)#,
-                      #nimble_plan,
-                      #report_plan,
-                      #validation_plan,
-                      #simulation_plan)
+the_plan = bind_plans(
+  raw_data_plan, 
+  depth_bin_imputation_plan,
+  dive_segmentation_plan,
+  nimble_plan
+  #report_plan,
+  #validation_plan,
+  #simulation_plan
+)
--- R/subplans/nimble_plan.R
+++ R/subplans/nimble_plan.R
@@ -17,26 +17,23 @@ nimble_plan = drake_plan(
   
   # reformat dive data for analysis in nimble (full dataset and validaiton set)
   nim_data = target(
-    flatten_tag_data(depth_files, dive_endpoints, template_bins, 
-                     tag_sex, cee_starts, validation_proportion),
-    transform = map(validation_proportion = c(0, .5)),
-    seed = 2020,
-    trigger = trigger(condition = FALSE, mode = 'blacklist')
+    flatten_tag_data(depth_files[1], dive_endpoints[1], template_bins, 
+                     tag_sex, cee_starts, validation_proportion = 0, 
+                     mcmc_sample_dir), 
+    format = 'file'
   ),
   
   # build priors and initialize parameters
   nim_pkg = target(
-    priors_and_inits(nim_data, template_bins, sattag_timestep, expm_delta),
-    transform = map(validation_proportion)#,
-  #   trigger = trigger(condition = FALSE, mode = 'blacklist')
+    priors_and_inits(nim_data, template_bins, sattag_timestep, expm_delta, 
+                     mcmc_sample_dir),
+    format = 'file'
   ),
-    
+
   # fit models
   mcmc_samples = target(
-    fit(nim_pkg, mcmc_sample_dir, niter, ncheckpoints, .id_chr),
-    transform = map(nim_pkg),
-    # format = 'file', 
-    trigger = trigger(condition = FALSE, mode = 'blacklist')
+    fit(nim_pkg, mcmc_sample_dir, niter, ncheckpoints),
+    format = 'file'
   )
   
 )
