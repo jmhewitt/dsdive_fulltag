c74f9feacc21ec381b5f30308049e98fd76dca11
Josh Hewitt <joshua.hewitt@duke.edu>
2020-09-08 12:04:30 -0600

    initial report for stage duration learning

--- R/subplans/report_plan.R
+++ R/subplans/report_plan.R
@@ -11,7 +11,35 @@ report_plan = drake_plan(
                     nim_pkg_tgt = 'nim_pkg',
                     nburn = nburn)
     )
-  )
+  ),
+  
+  # posterior_report_stagelearning = target(
+  #   rmarkdown::render(
+  #     knitr_in(!!file.path('reports', 'templates', 
+  #                          'posterior_diagnostics.Rmd')),
+  #     output_file = 'posterior_diagnostics_stagelearning.html',
+  #     output_dir = 'reports',
+  #     quiet = FALSE,
+  #     params = list(samples = mcmc_samples_stagelearning,
+  #                   nim_pkg_tgt = 'nim_pkg',
+  #                   nburn = nburn)
+  #   )
+  # ),
+  
+  posterior_durations_stagelearning = 
+    target(
+      rmarkdown::render(
+        knitr_in(!!file.path('reports', 'templates', 
+                             'posterior_stagelearning.Rmd')),
+        output_file = 'posterior_durations_stagelearning.html',
+        output_dir = 'reports',
+        quiet = FALSE,
+        params = list(samples = mcmc_samples_stagelearning,
+                      nim_pkg_tgt = 'nim_pkg',
+                      nburn = nburn)
+      ), 
+      trigger = trigger(condition = TRUE)
+    )
   
 )
 
