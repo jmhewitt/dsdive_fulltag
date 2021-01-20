e92367b5234b49f7e79908ea84f72681bd36e3e5
Josh Hewitt <joshua.hewitt@duke.edu>
2021-01-20 15:46:46 -0700

    initial commit of support function to discretize a value to a set with fixed spacing

---
+++ R/nimble/closestIndex.R
@@ -0,0 +1,24 @@
+closestIndex = nimble::nimbleFunction(
+  run = function(value = double(0), minval = double(0), stepsize = double(0),
+                 nvals = integer(0)) {
+    # Parameters:
+    #  value - continuous-space parameter to map to discrete set
+    #  minval - minimum value in discrete set
+    #  stepsize - spacing between values in discrete set
+    #  nvals - number of values in set
+    
+    returnType(integer(0))
+    
+    # determine "optimal" index
+    ind <- round((value - minval) / stepsize) + 1
+    
+    # constrain optimal index to be within the discrete set
+    if(ind < 0) {
+      ind <- 1
+    } else if(ind > nvals) {
+      ind <- nvals
+    }
+    
+    return(ind)
+  }
+)
