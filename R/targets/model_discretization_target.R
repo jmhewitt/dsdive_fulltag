2a8a7a82ca9616c83b88e66100719a05d431829c
Josh Hewitt <joshua.hewitt@duke.edu>
2021-01-26 15:10:11 -0700

    update formatting

--- R/targets/model_discretization_target.R
+++ R/targets/model_discretization_target.R
@@ -1,40 +1,40 @@
 model_discretization_target = list(
-  
+
   # discretization of parameter space
   tar_target(parameter_discretization, list(
-    
+
     # vertical speeds, by movement structure (i.e., stage)
     lambda = matrix(
-      rep(c(0, .05, 101), 3), 
+      rep(c(0, .05, 101), 3),
       ncol = 3, byrow = TRUE,
       dimnames = list(
-        rownames = c('descent', 'foraging', 'ascent'), 
+        rownames = c('descent', 'foraging', 'ascent'),
         colnames = c('min_val', 'stepsize', 'nvals')
       )
     ),
-    
+
     # directional preferences, by movement structure (i.e., stage)
     pi = matrix(
       c(0, .01, 51,
         0.5, 0, 1,
-        0.5, .01, 51), 
+        0.5, .01, 51),
       ncol = 3, byrow = TRUE,
       dimnames = list(
-        rownames = c('descent', 'foraging', 'ascent'), 
+        rownames = c('descent', 'foraging', 'ascent'),
         colnames = c('min_val', 'stepsize', 'nvals')
       )
     )
   )),
-  
+
   # transition matrices
   tar_target(
-    name = transition_matrices, 
+    name = transition_matrices,
     command = {
-      
-      # depth bin widths and count 
+
+      # depth bin widths and count
       n_bins = nrow(template_bins)
       bin_widths = 2 * template_bins$halfwidth
-      
+
       # range of model parameters to consider under each dive construction
       movement_types = c('descent', 'foraging', 'ascent')
       params = lapply(movement_types, function(mv_type) {
@@ -52,20 +52,20 @@ model_discretization_target = list(
         )
       })
       names(params) = movement_types
-      
+
       # compute CTMC transition matrix for this time duration
       tstep = sattag_timestep
-      
+
       # condensed transition matrices for movement structures
       as.numeric(do.call(cbind, mapply(function(params, stage) {
         apply(params, 1, function(r) {
           expm(tstep * buildInfinitesimalGenerator(
-            pi = r['pi'], lambda = r['lambda'], M = n_bins, stage = stage, 
+            pi = r['pi'], lambda = r['lambda'], M = n_bins, stage = stage,
             widths = bin_widths
           ))
         })
       }, params, 1:3, SIMPLIFY = FALSE)))
     }
   )
-  
+
 )
