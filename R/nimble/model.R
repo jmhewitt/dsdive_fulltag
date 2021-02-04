729bee2a3d1e455b33cd5dd5ba639568f16f2a84
jmhewitt <joshua.hewitt@duke.edu>
2021-02-04 11:26:26 -0700

    add stage transition model, and stage likelihood to nimble model config

--- R/nimble/model.R
+++ R/nimble/model.R
@@ -1,15 +1,21 @@
-modelCode = nimbleCode({
+modelCode = nimble::nimbleCode({
 
   # introduce tx. matrix entries wrt a "dummy" distribution as this is the more 
   # efficient way to introduce large amounts of reference data into the model
   transition_matrices[1:n_txmat_entries] ~ dflatvec(length = n_txmat_entries)
   
-  # TODO: give a proper prior and population-level model to stage transitions!
   # TODO: add covariates s.t. we only see certain types of transitions at surface
-  for(i in 1:n_subjects) {
-    for(j in 1:n_covariates) {
-      for(k in 1:n_stage_txs) {
-        betas_tx[j, k, i] ~ dnorm(mean = 0, sd = 1e2)
+  # TODO: soft-code hyper priors to allow sensitivity studies, etc.
+  # latent stage transition model random effects and population-level parameters
+  for(i in 1:n_covariates) {
+    for(j in 1:n_stage_txs) {
+      # priors for population-level coefficients and variability
+      betas_tx_mu[i,j] ~ dnorm(mean = 0, sd = 1e2)
+      betas_tx_var[i,j] ~ dinvgamma(shape = 2, rate = 1)
+      # hierarchical layer for individual-level coefficients
+      for(k in 1:n_subjects) {
+        betas_tx[i, j, k] ~ dnorm(mean = betas_tx_mu[i,j], 
+                                  var = betas_tx_var[i,j])
       }
     }
   }
@@ -34,15 +40,31 @@ modelCode = nimbleCode({
   # largest sampling unit is a sequence of depth bins
   for(seg_num in 1:n_segments) {
     
-    # # introduce covariates wrt a "dummy" distribution as this is the more 
-    # # efficient way to introduce large numbers of covariates into the model
+    # introduce covariates wrt a "dummy" distribution as this is the more
+    # efficient way to introduce large numbers of covariates into the model
     covariates[
       1:n_covariates,  segments[seg_num,1]:segments[seg_num,4]
     ] ~ dflatmat(nrow = n_covariates, 
                  ncol = segments[seg_num,4] - segments[seg_num,1] + 1)
     
-    # TODO: give a proper likelihood to stages!
-    # stages[segments[seg_num,1]:segments[seg_num,4]] ~ dflatvec()
+    # introduce support for latent stages wrt a "dummy" distribution as this is 
+    # the more efficient way to introduce large numbers of consts into model
+    stage_supports[
+      1:n_stages, segments[seg_num,1]:segments[seg_num,4]
+    ] ~ dflatmat(nrow = n_stages, ncol = 
+                   segments[seg_num,4] - segments[seg_num,1] + 1)
+    
+    # likelihood for latent stages
+    stages[segments[seg_num,1]:segments[seg_num,4]] ~ dstages(
+      betas = betas_tx[1:n_covariates, 1:n_stage_txs, segments[seg_num, 3]],
+      covariates = covariates[
+        1:n_covariates,  segments[seg_num,1]:segments[seg_num,4]
+      ],
+      stage_supports = stage_supports[
+        1:n_stages, segments[seg_num,1]:segments[seg_num,4]
+      ],
+      n_timepoints = segments[seg_num,2]
+    )
     
     # likelihood for depth bin observations
     depths[segments[seg_num,1]:segments[seg_num,4]] ~ dbins(
