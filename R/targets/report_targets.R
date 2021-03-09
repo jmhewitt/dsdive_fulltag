47fa3f454498ef8974786ec24b0de1a031927fd8
jmhewitt <joshua.hewitt@duke.edu>
2021-03-09 11:17:50 -0700

    add posterior stages to output; abstract calls

--- R/targets/report_targets.R
+++ R/targets/report_targets.R
@@ -1,6 +1,35 @@
 report_targets = list(
 
-  tarchetypes::tar_render(
+  # list of directories containing mcmc output
+  tarchetypes::tar_files(sample_dirs, dir(mcmc_sample_dir, full.names = TRUE)),
+  
+  # convergence and sampler diagnostics for model parameters
+  tarchetypes::tar_render_rep(
     name = parameter_diagnostics,
-    path = 'reports/templates/parameter_diagnostics.Rmd')
+    path = 'reports/parameter_diagnostics/parameter_diagnostics.Rmd',
+    params = data.frame(
+      tgt_dir = basename(sample_dirs),
+      output_file = paste(
+        'parameter_diagnostics_',
+        basename(sample_dirs),
+        '.pdf',
+        sep = ''
+      )
+    ),
+    deployment = 'worker'
+  ),
+  
+  # posterior plots for stages
+  tar_target(
+    name = posterior_imputation_plots,
+    command = imputation_plots(
+      mcmc_sample_dir = sample_dirs,
+      output_dir = file.path(posterior_stages_dir, basename(sample_dirs)),
+      template_bins = template_bins,
+      tag_info = tag_info
+    ), 
+    pattern = map(sample_dirs),
+    deployment = 'worker', 
+  )
+  
 )
