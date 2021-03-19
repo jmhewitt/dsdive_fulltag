a55f354542f63d6c6ecabd916e78258d93fb3545
jmhewitt <joshua.hewitt@duke.edu>
2021-03-19 10:04:04 -0600

    block-sample stage transition intercepts

--- R/nimble/fit.R
+++ R/nimble/fit.R
@@ -191,21 +191,24 @@ fit = function(nim_pkg, nsamples, nthin, max_batch_iter = Inf,
   #   }
   # }
   
-  # # use blocked samplers for stage transition effects between stages
-  # for(stage in nim_pkg$consts$ascent_like_stages) {
-  #   # stage transition effects associated with stage
-  #   stage_tx_inds = which(nim_pkg$consts$betas_tx_stage_from == stage)
-  #   # replace intercept random effect samplers with blocked samplers
-  #   for(k in 1:nim_pkg$consts$n_subjects) {
-  #     # identify nodes
-  #     tgt = paste('betas_tx_eps[', nim_pkg$consts$intercept_covariate, ', ', 
-  #                 stage_tx_inds, ', ', k, ']', sep = '')
-  #     # remove default samplers
-  #     conf$removeSamplers(tgt)
-  #     # add block RW samplers
-  #     conf$addSampler(target = tgt, type = 'RW_block')
-  #   }
-  # }
+  # use blocked samplers for stage transition effects between stages
+  for(stage in nim_pkg$consts$ascent_like_stages) {
+    # stage transition effects associated with stage
+    stage_tx_inds = which(nim_pkg$consts$betas_tx_stage_from == stage)
+    # replace intercept random effect samplers with blocked samplers
+    for(k in 1:nim_pkg$consts$n_subjects) {
+      # identify all potentially relevant nodes
+      tgt = paste('betas_tx[', nim_pkg$consts$intercept_covariate, ', ',
+                  stage_tx_inds, ', ', k, ']', sep = '')
+      # restrict target nodes to subset of actively sampled nodes 
+      tgt = tgt[tgt %in% sapply(conf$samplerConfs, function(s) s$target)]
+      # replace default samplers with block RW samplers if appropriate
+      if(length(tgt) > 1) {
+        conf$removeSamplers(tgt)
+        conf$addSampler(target = tgt, type = 'RW_block')
+      }
+    }
+  }
   
   # don't estimate population-level effects if so requested 
   if(nim_pkg$consts$population_effects == FALSE) {
