5e89812ec1659183201403a0f01eb5a0ff4c9da7
jmhewitt <joshua.hewitt@duke.edu>
2021-04-06 10:52:27 -0600

    initial commit of posterior predictive distribution for dive length, given a fixed dive stage

--- R/targets/cee_targets.R
+++ R/targets/cee_targets.R
@@ -21,6 +21,29 @@ cee_targets = list(
     deployment = 'worker',
     storage = 'worker',
     memory = 'transient'
+  ),
+  
+  tar_target(
+    cee_tag_targets_hypothetical_deep, 
+    c("ZcTag093", "ZcTag095", "ZcTag096", "ZcTag097")
+  ),
+  
+  tar_target(
+    name = dive_length_predictions_hypothetical_stage,
+    command = predict_dive_length_fixed_stage(
+      post_output_dir = file.path('output', 'mcmc', 'nim_fit'),
+      tag = cee_tag_targets,
+      burn = 1:1e3,
+      timestep = sattag_timestep/imputation_factor,
+      lon = cape_hatteras_loc['lon'],
+      lat = cape_hatteras_loc['lat'],
+      template_bins = template_bins,
+      fixed_stage = 1
+    ),
+    pattern = map(cee_tag_targets_hypothetical_deep),
+    deployment = 'worker',
+    storage = 'worker',
+    memory = 'transient'
   )
   
 )
