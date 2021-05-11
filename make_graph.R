1c7388ff5328d08c8318051df334c865c3586a29
jmhewitt <joshua.hewitt@duke.edu>
2021-05-11 08:23:47 -0600

    Update make_graph.R

--- make_graph.R
+++ make_graph.R
@@ -3,7 +3,17 @@ library(targets)
 # library(nimble)
 # source('_targets.R')
 
-# tar_make()
-
 # run workflow in current session, allowing for debugging breakpoints
-tar_make(callr_function = NULL)
+targets::tar_make(dive_length_predictions, callr_function = NULL)
+
+targets::tar_make(nim_fit, callr_function = NULL)
+
+targets::tar_make(parameter_diagnostics, callr_function = NULL)
+
+targets::tar_make_future(names = parameter_diagnostics, workers = 6)
+
+targets::tar_make_future(names = posterior_imputation_plots, workers = 6)
+
+targets::tar_make(names = posterior_imputation_plots, callr_function = NULL)
+
+targets::tar_make(names = mle_speeds, callr_function = NULL)
