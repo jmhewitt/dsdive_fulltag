f5a7c1de4a0385aa8fcee1af69f8b91cd57568b2
jmhewitt <joshua.hewitt@duke.edu>
2021-05-23 22:13:14 -0600

    update cee targets

--- R/targets/cee_targets.R
+++ R/targets/cee_targets.R
@@ -1,49 +1,123 @@
 cee_targets = list(
   
   tar_target(
-    cee_tag_targets, 
-    c("ZcTag069", "ZcTag085", "ZcTag086", "ZcTag093", 
-      "ZcTag095", "ZcTag096", "ZcTag097")
-  ),
-  
-  tar_target(
-    name = dive_length_predictions,
-    command = predict_dive_length(
-      post_output_dir = file.path('output', 'mcmc', 'nim_fit'),
-      tag = cee_tag_targets,
-      burn = 1:1e3,
-      timestep = sattag_timestep/imputation_factor,
-      lon = cape_hatteras_loc['lon'],
-      lat = cape_hatteras_loc['lat'],
-      template_bins = template_bins
-    ),
-    pattern = map(cee_tag_targets),
-    deployment = 'worker',
-    storage = 'worker',
-    memory = 'transient'
-  ),
-  
-  tar_target(
-    cee_tag_targets_hypothetical_deep, 
+    cee_dive_response_targets, 
     c("ZcTag093", "ZcTag095", "ZcTag096", "ZcTag097")
   ),
   
   tar_target(
-    name = dive_length_predictions_hypothetical_stage,
-    command = predict_dive_length_fixed_stage(
-      post_output_dir = file.path('output', 'mcmc', 'nim_fit'),
-      tag = cee_tag_targets_hypothetical_deep,
-      burn = 1:1e3,
-      timestep = sattag_timestep/imputation_factor,
-      lon = cape_hatteras_loc['lon'],
-      lat = cape_hatteras_loc['lat'],
-      template_bins = template_bins,
-      fixed_stage = 1
-    ),
-    pattern = map(cee_tag_targets_hypothetical_deep),
-    deployment = 'worker',
-    storage = 'worker',
-    memory = 'transient'
+    name = cee_dive_response_probs, 
+    command = {
+      
+      # location of MCMC files (also used as output path)
+      path = nim_fit
+      
+      # posterior parameter sample files
+      param_sample_files = dir(
+        path = path, pattern = 'parameter_samples_[0-9]+', full.names = TRUE
+      )
+      
+      # posterior stage sample files
+      stage_sample_files = dir(
+        path = path, pattern = 'stage_samples_[0-9]+', full.names = TRUE
+      )
+      
+      # column labels for posterior parameter samples
+      param_label_file = dir(
+        path = path, pattern = 'parameter_samples_column', full.names = TRUE
+      )
+      
+      # load posterior parameter samples 
+      param_samples = do.call(rbind, lapply(param_sample_files, function(f) {
+        readRDS(f)
+      }))
+      
+      # load posterior stage samples
+      stage_samples = do.call(rbind, lapply(stage_sample_files, function(f) {
+        readRDS(f)
+      }))
+      
+      # load data package
+      nim_pkg = readRDS(
+        file = dir(path = path, pattern = 'nim_pkg', full.names = TRUE)
+      )
+      
+      # label posterior parameter samples
+      colnames(param_samples) = readRDS(param_label_file)
+      
+      burn = 1:1e3
+      
+      # indices of posterior samples to process
+      post_samples = (1:nrow(param_samples))[-burn]
+      
+      # match tag id in model
+      subj_id = which(
+        cee_dive_response_targets == nim_pkg$consts$subject_id_labels
+      )
+      
+      # extract pre-exposure data index
+      pre_exposure_ind = data.frame(nim_pkg$consts$segments) %>% 
+        filter(subject_id == subj_id) %>% 
+        select(end_ind) %>% 
+        unlist() %>% 
+        max()
+      
+      # TODO: remove
+      # subset posterior samples according to parallelization task
+      post_samples = post_samples[1:100]
+      
+      # draw posterior predictive samples of time to next deep observation
+      post_pred_samples = sapply(post_samples, function(sample_ind) {
+          
+          # extract stage transition coefficients
+          betas_tx = array(dim = c(nim_pkg$consts$n_covariates,
+                                   nim_pkg$consts$n_stages,
+                                   nim_pkg$consts$n_stages - 1))
+          for(i in 1:nrow(betas_tx)) {
+            for(j in 1:ncol(betas_tx)) {
+              for(k in 1:dim(betas_tx)[3]) {
+                betas_tx[i,j,k] = param_samples[
+                  sample_ind, paste('beta_tx[', i, ', ', j, ', ', k, ']', 
+                                    sep = '')
+                ]
+              }
+            }
+          }
+          
+          # forward-simulate dive from predictive distribution
+          init_inds = seq(to = pre_exposure_ind, by = 1, length.out = 12)
+          fwd_sim = fwd_sim_to_deep(
+            stages = stage_samples[sample_ind, init_inds], 
+            depths = nim_pkg$data$depths[init_inds], 
+            covariates = nim_pkg$data$covariates[, init_inds], 
+            n_max = 1e3, 
+            nim_pkg = nim_pkg,
+            lambda = param_samples[sample_ind, c('lambda[1]', 'lambda[2]')], 
+            betas_tx = betas_tx, 
+            template_bins = template_bins, 
+            times = as.POSIXct(
+              x = nim_pkg$data$times[init_inds],
+              tz = 'UTC', 
+              origin = '1970-01-01 00:00.00 UTC'
+            ), 
+            timestep = sattag_timestep, 
+            lon = cape_hatteras_loc['lon'], lat = cape_hatteras_loc['lat'], 
+            depth_threshold = deep_dive_depth
+          )
+          
+          # sampled time to deep depth  
+          length(fwd_sim$depths) - 1
+          
+        })
+      
+      list(list(
+        tag = cee_dive_response_targets,
+        samples = post_pred_samples
+      ))
+      
+    }, 
+    pattern = map(cee_dive_response_targets)
   )
+  
   
 )
--- make_slurm.R
+++ make_slurm.R
@@ -1,6 +1,9 @@
 library(targets)
 
 tar_make_future(
-  names = c('validate_deep_surv_samples', 'dive_survival_eda_plot'), 
+  names = c('validate_deep_surv_samples', 'dive_survival_eda_plot',
+            'cee_dive_response_probs'), 
   workers = 400
 )
+
+tar_make(dive_survival_eda_plot, callr_function = NULL)
