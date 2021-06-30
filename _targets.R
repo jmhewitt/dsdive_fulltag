7913eb6b2408c56a3152081e6235a8edd81ad023
jmhewitt <joshua.hewitt@duke.edu>
2021-06-30 10:15:19 -0600

    update package dependencies

--- _targets.R
+++ _targets.R
@@ -14,7 +14,7 @@ if(system('command -v sbatch') == 0) {
 tar_option_set(
   packages = c('dplyr', 'lubridate', 'ggplot2', 'ggthemes', 'stringr', 
                'nimble', 'expm', 'pryr', 'suncalc', 'tarchetypes', 'coda',
-               'tidyr', 'future', 'future.batchtools', 'viridis'),
+               'tidyr', 'future', 'future.batchtools', 'viridis', 'splines2'),
   deployment = 'main'
 )
 
