23c87c9b6f572b9805eaf4a504bc1ef68f51d484
jmhewitt <joshua.hewitt@duke.edu>
2022-01-12 13:44:31 -0700

    update job execution for cluster

--- make.R
+++ make.R
@@ -1,1 +1,12 @@
-targets::tar_make_future(workers = 4)
+# fit model on single node
+targets::tar_make(
+  names = fit_marginalized_model,
+  callr_function = NULL
+)
+
+# # compute CEE probabilities in parallel
+# targets::tar_make_future(
+#   names = marginalized_model_cee_prediction,
+#   workers = 400,
+#   callr_function = NULL
+# )
