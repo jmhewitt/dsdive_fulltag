1d9e36b02bc0abaf8e32ecde152add04c2f9f565
jmhewitt <joshua.hewitt@duke.edu>
2022-03-31 12:09:35 -0600

    extend simulation to capture first two deep dives

--- R/generative/fwd_sim_dive.R
+++ R/generative/fwd_sim_dive.R
@@ -1,6 +1,6 @@
 fwd_sim_dive = function(stage, depth_bins, covariates, n_timepoints, nim_pkg,
-                        model, lon, lat, depth_threshold, template_bins, 
-                        subject_id, covariate_tx_control) {
+                        model, lon, lat, depth_threshold, shallow_threshold,
+                        template_bins, subject_id, covariate_tx_control) {
   # forward-sampling of dive 
   #
   # Parameters:
@@ -12,9 +12,16 @@ fwd_sim_dive = function(stage, depth_bins, covariates, n_timepoints, nim_pkg,
   #  model - nimble model containing transition matrices
   #  lon - longitude around which sampling occurs (for day/night calculations)
   #  lat - latitude around which sampling occurs (for day/night calculations)
-  #  depth_threshold - sampling ends after this depth is exceeded
+  #  depth_threshold - sampling ends after this depth is exceeded for "2nd" time
+  #  shallow_threshold - simulated dive must go below depth_threshold twice, 
+  #    with a visit above shallow_threshold in between in order for simulation
+  #    to end
   #  template_bins - for building out covariates
   #  subject_id - subject number id, so correct tx. mats can be extracted
+  
+  # initialize termination conditions
+  visited_deep = FALSE
+  recovered_after_deep = FALSE
   
   covariate_tx_control$lon = lon
   covariate_tx_control$lat = lat
@@ -30,9 +37,32 @@ fwd_sim_dive = function(stage, depth_bins, covariates, n_timepoints, nim_pkg,
     # check termination conditions
     #
     
+    ###########################################################################
+    #
+    #                       termination conditions
+    # 
+    # simple state machine model to terminate simulation once a recovery 
+    # cycle has been completed.  the cycle requires the simulated depth to 
+    # exceed the depth threshold, then visit a shallow, recovery depth, then 
+    # exceed the depth threshold once more.
+    # 
+    ###########################################################################
+    
+    # event: exceeded deep depth
     if(covariates['depth', ind] > depth_threshold) {
-      break
+      visited_deep = TRUE
+      if(recovered_after_deep) {
+        break
+      }
+    } 
+    
+    # event: in shallow, recovery depth
+    if(covariates['depth', ind] < shallow_threshold) {
+      if(visited_deep) {
+        recovered_after_deep = TRUE
+      }
     }
+    
     
     #
     # sample
@@ -70,23 +100,23 @@ fwd_sim_dive = function(stage, depth_bins, covariates, n_timepoints, nim_pkg,
     # sample stage
     #
     
-    # map the new covariate
-    covariateId = which(
-      apply(nim_pkg$consts$covariates_unique, 2, function(x) {
-        all(x == covariates_modeled[, ncol(covariates_modeled)])
-      })
-    )
+    # # map the new covariate
+    # covariateId = which(
+    #   apply(nim_pkg$consts$covariates_unique, 2, function(x) {
+    #     all(x == covariates_modeled[, ncol(covariates_modeled)])
+    #   })
+    # )
     
-    # extract the matching transition matrix, or rebuild (i.e., if it is new)
-    if(length(covariateId) == 1) {
-      txprobs = model$stage_tx_mat[subject_id, stage, , covariateId]
-    } else {
+    # # extract the matching transition matrix, or rebuild (i.e., if it is new)
+    # if(length(covariateId) == 1) {
+    #   txprobs = model$stage_tx_mat[subject_id, stage, , covariateId]
+    # } else {
       txprobs = stageTxMats(
         betas = model$beta_tx[subject_id, , , ],
         covariates = covariates_modeled[, ncol(covariates_modeled), drop = FALSE],
         n_timepoints = 1
       )[stage, , ]
-    }
+    # }
     
     # sample next stage
     stage = sample(
